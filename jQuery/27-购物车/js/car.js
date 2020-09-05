e77298ae01de483932564765f49d9699201e4a83
799953468 <799953468@qq.com>
2020-09-05 21:04:43 +0800

    Update

---
+++ jQuery/27-购物车/js/car.js
@@ -0,0 +1,13 @@
+$(function() {
+    // 全选
+    $(".checkall").change(function() {
+        $(".j-checkbox, .checkall").prop("checked", $(this).prop("checked"));
+    });
+    $(".j-checkbox").change(function() {
+        if ($(".j-checkbox:checked").length === $(".j-checkbox").length) {
+            $(".checkall").prop("checked", true);
+        } else {
+            $(".checkall").prop("checked", false);
+        }
+    });
+})
