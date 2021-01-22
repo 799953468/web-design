baf3ccd0700cb965ee50e5173e11f69164421a86
799953468 <799953468@qq.com>
2021-01-22 18:08:33 +0800

    Update

--- Ajax/server/app.js
+++ Ajax/server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const bodyparser = require('body-parser');
 const app = express();
 
@@ -22,7 +23,16 @@ app.post('/json', (req, res) => {
     res.send(req.body);
 });
 app.get('/error', (req, res) => {
+    console.log(abc);
     res.status(400).send('not ok');
+});
+app.get('/cache', (req, res) => {
+    fs.readFile('./test.txt', (err, result) => {
+        res.send(result);
+    })
+});
+app.get('/first', (req, res) => {
+    res.send('4');
 });
 app.listen(3000);
 console.log('服务器启动成功');
